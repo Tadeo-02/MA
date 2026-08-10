84f916023f9566e274fcbcd61ead9171b1306d7f
Tadeo Rufine <153774639+Tadeo-02@users.noreply.github.com>
2026-08-10 19:56:15 -0300

    Test Verde 2/3

--- Clase2/main.py
+++ Clase2/main.py
@@ -1,15 +1,17 @@
 
 #En grupos. Desarrollan la funcionalidad que les pido del String Calculator. Usando TDD. Haciendo Push en cada Verde.
 #Cuando terminan la funcionalidad, avisan en el canal de chat del grupo y yo les pido mas funcionalidad.
-import unittest
+def sumar(numeros):
+    return sum(map(int, numeros.split(",")))
 
-def test_suma_dos_numeros():
+
+def test_sumar_dos_numeros():
     assert sumar("1,2") == 3
 
-def test_suma_vacio():
+def test_sumar_vacio():
     assert sumar("") == 0
 
-def test_suma_un_numero():
+def test_sumar_un_numero():
     assert sumar("1") == 1
 
 
