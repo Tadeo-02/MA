894aaf96355a70eb116b2579cdcf5a03e57fa005
Tadeo <tadeorufine02@gmail.com>
2026-08-24 19:59:43 -0300

    GREEN: girar izquierda girar derecha

--- Mars-Rover/main_rover.py
+++ Mars-Rover/main_rover.py
@@ -1,2 +1,7 @@
 def informar_posicion_orientacion(x, y, orientacion):
     return f"{x},{y},{orientacion}"
+def girar(direccion):
+    if (direccion == "L"):
+        return "O"
+    elif (direccion == "R"):
+        return "N"
--- Mars-Rover/test_rover.py
+++ Mars-Rover/test_rover.py
@@ -1,5 +1,4 @@
-from main_rover import informar_posicion_orientacion
-
+from main_rover import informar_posicion_orientacion, girar
 def test_informar_posicion_orientacion():
     assert informar_posicion_orientacion(1,2,"N") == "1,2,N"
 def test_girar():
