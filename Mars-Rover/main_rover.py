fa8ef586bc1ace288a6af11beaca37c7ade2b738
matimorici <morici.mati@gmail.com>
2026-08-24 20:10:49 -0300

    REFACTOR: generalizar giro del rover según orientación actual

--- Mars-Rover/main_rover.py
+++ Mars-Rover/main_rover.py
@@ -1,7 +1,13 @@
 def informar_posicion_orientacion(x, y, orientacion):
     return f"{x},{y},{orientacion}"
+
+orientacion_actual = "N"
 def girar(direccion):
+    global orientacion_actual
+    orientaciones = ["N", "E", "S", "O"]
+    direccion_actual = orientaciones.index(orientacion_actual)
     if (direccion == "L"):
-        return "O"
+        orientacion_actual = orientaciones[(direccion_actual - 1) % 4]
     elif (direccion == "R"):
-        return "N"
+        orientacion_actual = orientaciones[(direccion_actual + 1) % 4]
+    return orientacion_actual
--- Mars-Rover/test_rover.py
+++ Mars-Rover/test_rover.py
@@ -1,4 +1,5 @@
 from main_rover import informar_posicion_orientacion, girar
+
 def test_informar_posicion_orientacion():
     assert informar_posicion_orientacion(1,2,"N") == "1,2,N"
 def test_girar():
