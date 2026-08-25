ac19cbb6d0e3c4ebb8480e0da346d652d6f19a71
Tadeo <tadeorufine02@gmail.com>
2026-08-24 21:09:13 -0300

    REFACTOR: extraer cálculo de desplazamiento a _delta y agregar moverse con acumulación real de posición

--- Mars-Rover/main_rover.py
+++ Mars-Rover/main_rover.py
@@ -1,45 +1,48 @@
+ORIENTACIONES = ["N", "E", "S", "O"]
+DELTAS = {"N": (0, 1), "E": (1, 0), "S": (0, -1), "O": (-1, 0)}
+
+orientacion_actual = "N"
+pos_actual = [0, 0]
+
 def informar_posicion_orientacion(x, y, orientacion):
     return f"{x},{y},{orientacion}"
 
-orientacion_actual = "N"
 def girar(direccion):
     global orientacion_actual
-    orientaciones = ["N", "E", "S", "O"]
-    direccion_actual = orientaciones.index(orientacion_actual)
-    if (direccion == "L"):
-        orientacion_actual = orientaciones[(direccion_actual - 1) % 4]
-    elif (direccion == "R"):
-        orientacion_actual = orientaciones[(direccion_actual + 1) % 4]
+    idx = ORIENTACIONES.index(orientacion_actual)
+    if direccion == "L":
+        orientacion_actual = ORIENTACIONES[(idx - 1) % 4]
+    elif direccion == "R":
+        orientacion_actual = ORIENTACIONES[(idx + 1) % 4]
     return orientacion_actual
 
+def _delta(signo):
+    dx, dy = DELTAS[orientacion_actual]
+    return dx * signo, dy * signo
+
 def avanzar(direccion):
-    pos_actual = [0, 0]
-
-    if orientacion_actual == "N":
-        pos_actual[1] += 1
-    elif orientacion_actual == "E":
-        pos_actual[0] += 1
-    elif orientacion_actual == "O":
-        pos_actual[0] -= 1
-    else:
-        pos_actual[1] -= 1
-
-    return tuple(pos_actual)
-
+    return _delta(1)
 
 def retroceder(direccion):
-    pos_actual = [0, 0]
+    return _delta(-1)
 
-    if orientacion_actual == "N":
-        pos_actual[1] -= 1
-    elif orientacion_actual == "E":
-        pos_actual[0] -= 1
-    elif orientacion_actual == "O":
-        pos_actual[0] += 1
-    else:
-        pos_actual[1] += 1
-
-    return tuple(pos_actual)
+def moverse(comandos):
+    global pos_actual
+    for c in comandos:
+        if c in ("L", "R"):
+            girar(c)
+        elif c == "F":
+            dx, dy = _delta(1)
+            pos_actual[0] += dx
+            pos_actual[1] += dy
+        elif c == "B":
+            dx, dy = _delta(-1)
+            pos_actual[0] += dx
+            pos_actual[1] += dy
+    return tuple(pos_actual) + (orientacion_actual,)
 
 def cadena_comandos(comandos):
-    return (2, 2, "E")
+    global pos_actual, orientacion_actual
+    pos_actual = [0, 0]
+    orientacion_actual = "N"
+    return moverse(comandos)
